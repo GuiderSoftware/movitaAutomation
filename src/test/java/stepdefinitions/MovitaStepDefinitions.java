29bb16769bebc294209c8f7d6e9d5c25cd9b2554
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-10-09 22:35:13 +0300

    denemeYENİ

--- src/test/java/stepdefinitions/MovitaStepDefinitions.java
+++ src/test/java/stepdefinitions/MovitaStepDefinitions.java
@@ -35,6 +35,5 @@ public class MovitaStepDefinitions {
        Assert.assertTrue(movita.mainTextTurkish.isDisplayed());
     }
 
-    //deneme
-    // Alkan Tuncer Deneme
+   //Hüseyin Kartal deneme 10.34
 }
